020e9aa85f568e6cb2cd645c010e3557b3b38533
yongelee <albertkim91@gmail.com>
2018-10-22 10:11:58 -0400

    changed to ga again

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,33 +4,25 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-amplitude-analytics`,
-      options: {
-        // Specify the API key for your Amplitude Project (required)
-        apiKey: '	f799f85cd19544037e4c8f39e89cbd4b',
-        // Puts tracking script in the head instead of the body (optional)
-        head: true,
-        // Prevents loading Amplitude and logging events if visitors have "Do Not Track" enabled (optional)
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths (optional)
-        exclude: [],
-        // Override the default event types (optional)
-        eventTypes: {
-          outboundLinkClick: 'OUTBOUND_LINK_CLICK',
-          pageView: 'PAGE_VIEW'
-        },
-        // Amplitude JS SDK configuration options (optional)
-        amplitudeConfig: {
-          saveEvents: true,
-          includeUtm: true,
-          includeReferrer: true
-        }
-      }
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-77921789-9',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [''],
+        // Any additional create only fields (optional)
+        cookieDomain: 'whattopack.app'
       }
     },
     'gatsby-plugin-react-helmet',
